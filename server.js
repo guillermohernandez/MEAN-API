2e4cc5c3d96d4f4f7312d6380b23e8bb36d04fd9
Guillermo Hernandez <guillermohernandez@users.noreply.github.com>
2015-12-30 02:05:23 -0600

    set up server, database, and product model

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+
+// required packages
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://memo:zx3kQcvr@ds056688.mongolab.com:56688/mean-api'); // connect to our database
+var Product    = require('./app/models/product');
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'Success!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Connected on port ' + port);
